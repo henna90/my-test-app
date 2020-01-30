1a62b00d6d8bba9714eeb0c55929c68f3cbe31cc
Henna Abbas <hennaabbas@hennas-mbp.lan>
2020-01-29 16:44:19 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from './Button'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+       
+          My Test App
+      
+      </header>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
